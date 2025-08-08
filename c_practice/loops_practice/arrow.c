6ac004b61979110711e9c1b6dc0c59a7f53bc390
maheshtankala <maheshtankala276@gmail.com>
2025-08-08 17:26:57 +0530

    loops

---
+++ c_practice/loops_practice/arrow.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+int i,j;
+for(i=1;i<=3;i++){
+	for(j=1;j<=(3+i);j++){
+		printf(" ");}
+	printf("*\n");
+}
+for(i=1;i<9;i++){
+	printf("*");}
+	printf("\n");
+
+for(i=1;i<=3;i++){
+	for(j=1;j<=(7-i);j++){
+	printf(" ");
+	}
+	printf("*\n");}
+}
+
+
