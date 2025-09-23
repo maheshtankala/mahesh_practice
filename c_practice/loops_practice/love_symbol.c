4778c9fae1f8d232b8a54566ba270afd861a0756
maheshtankala <maheshtankala276@gmail.com>
2025-09-23 19:34:42 +0530

    patterns

---
+++ c_practice/loops_practice/love_symbol.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+	int i,j,k,n;
+	printf("Enter the number: ");
+	scanf("%d",&n);
+	for(i=1;i<=n/2;i++){
+		for(j=1;j<=n/2-i;j++){
+			printf(" ");
+		}
+		for(k=1;k<=2*i-1;k++){
+			printf("*");
+		}
+		for(int t=1;t<=2*(n/2-i);t++){
+			printf(" ");
+		}
+		for(int r=1;r<=2*i-1;r++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	for(i=1;i<=n;i++){
+		for(j=1;j<i;j++){
+			printf(" ");
+		}
+		for(k=1;k<=(2*n)-(2*i);k++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
+       
+
